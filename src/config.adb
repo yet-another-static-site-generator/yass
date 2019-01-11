1693b5d6d5202f6507a411e2408ba6bbed5d7df8
Bartek thindil Jasicki <thindil@laeran.pl>
2019-01-11 07:51:19 +0100

    added creation of basic config file

---
+++ src/config.adb
@@ -0,0 +1,36 @@
+--    Copyright 2019 Bartek thindil Jasicki
+--
+--    This file is part of YASS.
+--
+--    YASS is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    YASS is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with YASS.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+package body Config is
+
+   procedure CreateConfig(DirectoryName: String) is
+      ConfigFile: File_Type;
+   begin
+      Create(ConfigFile, Append_File, DirectoryName & "/site.cfg");
+      Put_Line(ConfigFile, "LayoutsDirectory = _layouts");
+      Put_Line(ConfigFile, "OutputDirectory = _output");
+      Put_Line(ConfigFile, "# Site tags");
+      Put_Line(ConfigFile, "Name = New Site");
+      Close(ConfigFile);
+      YassConfig :=
+        (LayoutsDirectory => To_Unbounded_String("_layouts"),
+         OutputDirectory => To_Unbounded_String("_output"));
+   end CreateConfig;
+
+end Config;
