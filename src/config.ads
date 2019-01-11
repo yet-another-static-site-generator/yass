9ec93645817f40d650b57f731d92e8a4fdb2cda2
thindil@laeran.pl <thindil@laeran.pl>
2019-01-11 06:51:18 +0000

    added creation of basic config file
    FossilOrigin-Name: be552392f055d42095939a8007279e85de6715539ae34b92829d05766306e868

---
+++ src/config.ads
@@ -0,0 +1,37 @@
+--    Copyright 2019 Bartek thindil Jasicki
+--
+--    This file is part of YASS.
+--
+--    YASS is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    YASS is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with YASS.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Strings.Hash;
+
+package Config is
+
+   type ParserConfig is -- Data structure for setting for parser
+   record
+      OutputDirectory: Unbounded_String; -- Path to directory with generated site
+      LayoutsDirectory: Unbounded_String; -- Path to directory where site layouts are
+   end record;
+   YassConfig: ParserConfig; -- Settings for parser
+   package Tags_Container is new Ada.Containers.Indefinite_Hashed_Maps(String,
+      String, Ada.Strings.Hash, "=");
+   SiteTags: Tags_Container.Map; -- Site tags (like title, author, etc)
+
+   procedure CreateConfig
+     (DirectoryName: String); -- Create default config in selected directory
+
+end Config;
