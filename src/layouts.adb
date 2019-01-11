c9f0cd700e56861e0029968a66c5d00f0ec5bda1
Bartek thindil Jasicki <thindil@laeran.pl>
2019-01-11 21:50:22 +0100

    added creating default layout

---
+++ src/layouts.adb
@@ -0,0 +1,38 @@
+--    Copyright 2019 Bartek thindil Jasicki
+--
+--    This file is part of YASS.
+--
+--    YASS is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    YASS is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with YASS.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+package body Layouts is
+
+   procedure CreateLayout(DirectoryName: String) is
+      LayoutFile: File_Type;
+   begin
+      Create(LayoutFile, Append_File, DirectoryName & "/_layouts/default.html");
+      Put_Line(LayoutFile, "<!DOCTYPE html>");
+      Put_Line(LayoutFile, "<html>");
+      Put_Line(LayoutFile, "<head>");
+      Put_Line(LayoutFile, "<title>{%Name%}</title>");
+      Put_Line(LayoutFile, "</head>");
+      Put_Line(LayoutFile, "<body>");
+      Put_Line(LayoutFile, "{%Contents%}");
+      Put_Line(LayoutFile, "</body>");
+      Put_Line(LayoutFile, "</html>");
+      Close(LayoutFile);
+   end CreateLayout;
+
+end Layouts;
