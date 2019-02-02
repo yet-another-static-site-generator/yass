2a459b086d5db4a4b841b61827fdf6437df5c9f7
thindil@laeran.pl <thindil@laeran.pl>
2019-02-02 12:12:15 +0000

    moved server code to separated package
    FossilOrigin-Name: 1281ff537b784be72117132b6c9f784ae32542259f397df8ee2b11b789c8546e

--- src/yass.adb
+++ src/yass.adb
@@ -30,6 +30,7 @@ with AWS.Services.Page_Server;
 with Config; use Config;
 with Layouts; use Layouts;
 with Pages; use Pages;
+with Server; use Server;
 
 procedure YASS is
    Version: constant String := "0.3";
@@ -73,89 +74,6 @@ procedure YASS is
       when GenerateSiteException =>
          return False;
    end BuildSite;
-
-   task MonitorSite is
-      entry Start;
-   end MonitorSite;
-
-   task body MonitorSite is
-      SiteRebuild: Boolean;
-      procedure MonitorDirectory(Name: String) is
-         procedure ProcessFiles(Item: Directory_Entry_Type) is
-            SiteFileName: Unbounded_String :=
-              To_Unbounded_String(Full_Name(Item));
-         begin
-            if YassConfig.ExcludedFiles.Find_Index(Simple_Name(Item)) /=
-              Excluded_Container.No_Index then
-               return;
-            end if;
-            Insert
-              (SiteFileName, Length(SiteDirectory) + 1,
-               Dir_Separator & To_String(YassConfig.OutputDirectory));
-            if Extension(Simple_Name(Item)) = "md" then
-               SiteFileName :=
-                 To_Unbounded_String
-                   (Compose
-                      (Containing_Directory(To_String(SiteFileName)),
-                       Ada.Directories.Base_Name(To_String(SiteFileName)),
-                       "html"));
-            end if;
-            if not Exists(To_String(SiteFileName)) then
-               if Extension(Simple_Name(Item)) = "md" then
-                  CreatePage(Full_Name(Item), Name);
-               else
-                  CopyFile(Full_Name(Item), Name);
-               end if;
-               Put_Line("File: " & To_String(SiteFileName) & " was added.");
-               SiteRebuild := True;
-            end if;
-            if Modification_Time(Full_Name(Item)) >
-              Modification_Time(To_String(SiteFileName)) then
-               if Extension(Simple_Name(Item)) = "md" then
-                  CreatePage(Full_Name(Item), Name);
-               else
-                  CopyFile(Full_Name(Item), Name);
-               end if;
-               Put_Line("File: " & To_String(SiteFileName) & " was updated.");
-               SiteRebuild := True;
-            end if;
-         end ProcessFiles;
-         procedure ProcessDirectories(Item: Directory_Entry_Type) is
-         begin
-            if YassConfig.ExcludedFiles.Find_Index(Simple_Name(Item)) =
-              Excluded_Container.No_Index then
-               MonitorDirectory(Full_Name(Item));
-            end if;
-         exception
-            when Ada.Directories.Name_Error =>
-               null;
-         end ProcessDirectories;
-      begin
-         Search
-           (Name, "", (Directory => False, others => True),
-            ProcessFiles'Access);
-         Search
-           (Name, "", (Directory => True, others => False),
-            ProcessDirectories'Access);
-      exception
-         when GenerateSiteException =>
-            Put_Line("Site rebuilding has been interrupted.");
-      end MonitorDirectory;
-   begin
-      select
-         accept Start;
-         loop
-            SiteRebuild := False;
-            MonitorDirectory(To_String(SiteDirectory));
-            if SiteRebuild then
-               Put_Line("Site was rebuild.");
-            end if;
-            delay 5.0;
-         end loop;
-      or
-         terminate;
-      end select;
-   end MonitorSite;
 
    function ValidArguments(Message: String; Exist: Boolean) return Boolean is
    begin
