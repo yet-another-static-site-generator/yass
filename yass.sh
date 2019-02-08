abe0da219a2b5b389944b02b2adbc3ebe6b59d8b
Bartek thindil Jasicki <thindil@laeran.pl>
2019-02-08 10:11:24 +0100

    removed startup script

--- yass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Very simple startup script. The only thing which it do is setting LD_LIBRARY_PATH environment variable. Use this script only if you use downloaded compiled version of YASS. In other way just run yass executable file.
-
-LD_LIBRARY_PATH=. ./yass "$@"
