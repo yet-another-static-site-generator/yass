ba74fb8115cb46ff5985a0e23ca2c610fdcfa0b0
thindil@laeran.pl <thindil@laeran.pl>
2019-03-17 08:34:46 +0000

    updated version info
    FossilOrigin-Name: e36250934fedff1d004f73b6ea08f7f145e8c1b191d781515ec2448baf26df63

--- src/yass.adb
+++ src/yass.adb
@@ -38,7 +38,7 @@ with Sitemaps; use Sitemaps;
 with AtomFeed; use AtomFeed;
 
 procedure YASS is
-   Version: constant String := "0.8";
+   Version: constant String := "1.0";
    WorkDirectory: Unbounded_String;
 
    function BuildSite(DirectoryName: String) return Boolean is
@@ -139,7 +139,7 @@ begin
         ("createfile [name] - create new empty markdown file with ""name""");
    elsif Argument(1) = "version" then
       Put_Line("Version: " & Version);
-      Put_Line("Released: 2019-03-17");
+      Put_Line("Released: not yet");
    elsif Argument(1) = "license" then
       Put_Line("Copyright (C) 2019 Bartek thindil Jasicki");
       New_Line;
