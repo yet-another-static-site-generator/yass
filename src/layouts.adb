0c007a2e9ee004f1b66cba1e5d5d0b912059330d
Bartek thindil Jasicki <thindil@laeran.pl>
2019-10-19 07:08:20 +0200

    added Atom link to default page template

--- src/layouts.adb
+++ src/layouts.adb
@@ -33,6 +33,9 @@ package body Layouts is
       Put_Line(LayoutFile, "<html>");
       Put_Line(LayoutFile, "<head>");
       Put_Line(LayoutFile, "<meta charset=""UTF-8"">");
+      Put_Line(LayoutFile, "@@IF@@ not {%AtomLink%} = """"");
+      Put_Line(LayoutFile, "{%AtomLink%}");
+      Put_Line(LayoutFile, "@@END_IF@@");
       Put_Line(LayoutFile, "<title>{%Name%}</title>");
       Put_Line(LayoutFile, "</head>");
       Put_Line(LayoutFile, "<body>");
