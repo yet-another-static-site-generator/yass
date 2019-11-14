1b6caaba8369d0d4d6498c6a6818c571c05c1bf1
thindil@laeran.pl <thindil@laeran.pl>
2019-11-14 06:59:22 +0000

    updated build script
    FossilOrigin-Name: c397d7d974ea3b27fe5142ca2851475d2185675f81924b3e70ddad3bfc671bbf

--- others/build.sh
+++ others/build.sh
@@ -5,8 +5,10 @@ releasedir=usr
 
 gprbuild -p yass.gpr -XMode=release
 mkdir -p $releasedir/share/doc/yass/
+mkdir -p $releasedir/share/metainfo/
 mkdir -p $releasedir/bin
 cp bin/yass $releasedir/bin
 cp README.md $releasedir/share/doc/yass
 cp COPYING $releasedir/share/doc/yass
 cp CONTRIBUTING.md $releasedir/share/doc/yass
+cp others/yass.appdata.xml $releasedir/share/metainfo
