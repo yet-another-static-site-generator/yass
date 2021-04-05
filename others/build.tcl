f5ed51ebdb09259768f01d406f5e366fa5ba9c09
thindil <thindil@laeran.pl>
2021-04-05 05:57:34 +0000

    added build Windows version to build script
    FossilOrigin-Name: 726ddab675dcc9aa470058224ebf95717f374f15074ac3bbc4bfd72b19a7fbfe

--- others/build.tcl
+++ others/build.tcl
@@ -7,18 +7,47 @@ if {[file exists yass.gpr] == 0} {
    return
 }
 
-# directory where the release will be created
-set releasedir usr
+# Set the target for the compilation. If no arguments, use system default
+if {$argc == 0} {
+   if {$tcl_platform(os) == "Linux"} {
+      set target x86_64-linux-gnu
+   } else {
+      set target x86_64-windows
+   }
+} else {
+   set target [lindex $argv 0]
+}
 
-exec gprclean -P yass.gpr >@stdout
-exec gprbuild -p -P yass.gpr -XMode=release >@stdout
+# Check if correct target was set
+if {$target != "x86_64-linux-gnu" && $target != "x86_64-windows"} {
+   puts {Invalid compilation target. Allowed options are x86_64-linux-gnu and x86_64-windows}
+   return
+}
+
+# Set the directory where the release will be created
+if {$target == "x86_64-linux-gnu"} {
+   set releasedir usr
+} else {
+   set releasedir release
+}
+
+exec gprclean -P yass.gpr --target=$target >@stdout
+exec gprbuild -p -P yass.gpr -XMode=release --target=$target >@stdout
 puts -nonewline {Copying files and directories ... }
-file mkdir $releasedir/share/doc/yass
-file mkdir $releasedir/share/metainfo/
-file copy bin $releasedir/
-file copy COPYING $releasedir/share/doc/yass/
-file copy README.md $releasedir/share/doc/yass/
-file copy CONTRIBUTING.md $releasedir/share/doc/yass/
-file copy others/yass.appdata.xml $releasedir/share/metainfo
+if {$target == "x86_64-linux-gnu"} {
+   file mkdir $releasedir/share/doc/yass
+   file mkdir $releasedir/share/metainfo/
+   file copy bin $releasedir/
+   file copy COPYING $releasedir/share/doc/yass/
+   file copy README.md $releasedir/share/doc/yass/
+   file copy CONTRIBUTING.md $releasedir/share/doc/yass/
+   file copy others/yass.appdata.xml $releasedir/share/metainfo
+} else {
+   file mkdir $releasedir
+   file copy [file join bin yass.exe] $releasedir
+   file copy COPYING $releasedir
+   file copy README.md $releasedir
+   file copy CONTRIBUTING.md $releasedir
+}
 puts {done}
-exec gprclean -P yass.gpr >@stdout
+exec gprclean -P yass.gpr --target=$target >@stdout
