83da701994fcfa339022fcc50eb8b24c0e6a13a8
thindil <thindil@laeran.pl>
2021-08-03 05:11:07 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: 10f31500722f2eac5dc3585b3fc17aa2dc9aa24b3d01a907a2dbfa68193c6dc3

--- src/modules.ads
+++ src/modules.ads
@@ -15,7 +15,7 @@
 --    You should have received a copy of the GNU General Public License
 --    along with YASS.  If not, see <http://www.gnu.org/licenses/>.
 
-with Config; use Config;
+with Config; use Config; --## rule line off REDUCEABLE_SCOPE
 
 -- ****h* Yass/Modules
 -- FUNCTION
