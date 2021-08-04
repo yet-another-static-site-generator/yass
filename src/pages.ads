b7a9ee57a800b56fccf1fea675dd15dab0d9ea39
thindil <thindil@laeran.pl>
2021-08-04 05:07:25 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: 30b6cbd397099cf999cd59ab807d0c7f091da55b77c75597429738e1a9265e6a

--- src/pages.ads
+++ src/pages.ads
@@ -22,61 +22,61 @@
 package Pages is
 -- ****
 
-   -- ****e* Pages/Pages.GenerateSiteException
+   -- ****e* Pages/Pages.Generate_Site_Exception
    -- FUNCTION
    -- Raised when generating site was interrupted
    -- SOURCE
-   GenerateSiteException: exception;
+   Generate_Site_Exception: exception;
    -- ****
 
-   -- ****f* Pages/Pages.CreatePage
+   -- ****f* Pages/Pages.Create_Page
    -- FUNCTION
    -- Create page from file with full path FileName in directory with full
    -- path Directory
    -- PARAMETERS
-   -- FileName  - Full path to the Markdown file which will be processed
+   -- File_Name - Full path to the Markdown file which will be processed
    -- Directory - Full path to the directory where newly create HTML file will
    --             be added
    -- SOURCE
-   procedure CreatePage(FileName, Directory: String) with
-      Pre => FileName'Length > 0 and Directory'Length > 0,
+   procedure Create_Page(File_Name, Directory: String) with
+      Pre => File_Name'Length > 0 and Directory'Length > 0,
       Test_Case => (Name => "Test_Create_Page", Mode => Nominal);
    -- ****
 
-   -- ****f* Pages/Pages.CopyFile
+   -- ****f* Pages/Pages.Copy_File
    -- FUNCTION
    -- Copy file with full path FileName to proper location in site output
    -- directory with full path Directory
    -- PARAMETERS
-   -- FileName  - Full path to the file which will be copied
+   -- File_Name - Full path to the file which will be copied
    -- Directory - Full path to the directory where file will be added
    -- SOURCE
-   procedure CopyFile(FileName, Directory: String) with
-      Pre => FileName'Length > 0 and Directory'Length > 0,
+   procedure Copy_File(File_Name, Directory: String) with
+      Pre => File_Name'Length > 0 and Directory'Length > 0,
       Test_Case => (Name => "Test_Copy_File", Mode => Nominal);
    -- ****
 
-   -- ****f* Pages/Pages.CreateEmptyFile
+   -- ****f* Pages/Pages.Create_Empty_File
    -- FUNCTION
    -- Create empty markdown file with default tags and with full path FileName
    -- PARAMETERS
-   -- FileName - Full path to the Markdown file which will be created
+   -- File_Name - Full path to the Markdown file which will be created
    -- SOURCE
-   procedure CreateEmptyFile(FileName: String) with
-      Pre => FileName'Length > 0,
+   procedure Create_Empty_File(File_Name: String) with
+      Pre => File_Name'Length > 0,
       Test_Case => (Name => "Test_Create_Empty_File", Mode => Nominal);
    -- ****
 
-   -- ****f* Pages/Pages.GetLayoutName
+   -- ****f* Pages/Pages.Get_Layout_Name
    -- FUNCTION
    -- Get name of layout used in the page with full path FileName
    -- PARAMETERS
-   -- FileName - Full path to the file which name layout will be check
+   -- File_Name - Full path to the file which name layout will be check
    -- RESULT
    -- Name of the layout used by the file
    -- SOURCE
-   function GetLayoutName(FileName: String) return String with
-      Pre => FileName'Length > 0,
+   function Get_Layout_Name(File_Name: String) return String with
+      Pre => File_Name'Length > 0,
       Test_Case => (Name => "Test_Get_Layout_Name", Mode => Nominal);
    -- ****
 
