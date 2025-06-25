13ccce04be051eec33de07eeb9334667ee464e69
Jesper Quorning <jesper.quorning@gmail.com>
2025-06-25 07:12:02 +0200

    Add preambles and RoboDoc

--- src/cmark.adb
+++ src/cmark.adb
@@ -1,3 +1,19 @@
+--    Copyright 2019-2021 Bartek thindil Jasicki & 2025 J. Quorning
+--
+--    This file is part of YASS.
+--
+--    YASS is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    YASS is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with YASS.  If not, see <http://www.gnu.org/licenses/>.
 
 with Interfaces.C.Strings;
 
